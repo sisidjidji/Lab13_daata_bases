6b15a7901d66099d403ee405c3c4a9267c10fd8f
sihem azibi <s.azibi@live.fr>
2020-05-20 00:49:15 -0500

    commit scafolding

---
+++ lab_13_data/Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_13_data
+{
+    public class HomeController: Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab_13_data/Data/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_13_data.Data
+{
+    public class DbContext
+    {
+    }
+}
