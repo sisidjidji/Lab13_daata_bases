b6bc6225f106bc4cfac12f50c1ccac182380d6e5
sihem azibi <s.azibi@live.fr>
2020-05-21 02:48:21 -0500

    hotelroom class and room class done

---
+++ lab_13_data/Models/HotelRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_13_data.Models
+{
+    public class HotelRoom
+    {
+
+        public int RoomNumber { get; set; }
+        public Room Room { get; set; }
+        public Hotel Hotel { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFrindly { get; set; }
+    }
+}
