98064e3ae8c83dd1ad15b6a4b933f301d2bd6466
sihem azibi <s.azibi@live.fr>
2020-05-21 01:05:03 -0500

    database setting done

--- lab_13_data/Data/DbContext.cs
+++ lab_13_data/Data/DbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace lab_13_data.Data
 {
-    public class DbContext
+    public class SchoolDbContext : DbContext
     {
+        public SchoolDbContext( DbContextOptions options) : base(options)
+        {
+        }
     }
 }
--- lab_13_data/Startup.cs
+++ lab_13_data/Startup.cs
@@ -2,21 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lab_13_data.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace lab_13_data
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();    
+            services.AddMvc();
+            services.AddDbContext<SchoolDbContext>(options => {
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
