9f15f0a489d92dd325ecdc5591b5ebd631e77dee
sihem azibi <s.azibi@live.fr>
2020-05-21 21:46:14 -0500

    home controller and ameneties controler done

--- lab_13_data/Controllers/HomeController.cs
+++ lab_13_data/Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace lab_13_data
+namespace lab_13_data.Controllers
 {
-    public class HomeController: Controller
+    public class HomeController : Controller
     {
        public IActionResult Index()
         {
