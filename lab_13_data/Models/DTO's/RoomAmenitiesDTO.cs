e39aa7daffc01f1e87dbbbff3efcd0775f1d3c3d
sihem azibi <s.azibi@live.fr>
2020-05-27 23:44:50 -0500

    creating dto folder with dto classes for each table

---
+++ lab_13_data/Models/DTO's/RoomAmenitiesDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_13_data.Models.DTO_s
+{
+    public class RoomAmenitiesDTO
+    {
+    }
+}
